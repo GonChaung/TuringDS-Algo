97be8fbb38e1e14f434dfbf92d23e5c00428c972
Pandas-12 <thantsithu.naing@icloud.com>
2024-09-03 15:25:03 +0700

    leetCode getLucky

--- src/main/java/leetCode/Solution.java
+++ src/main/java/leetCode/Solution.java
@@ -16,10 +16,27 @@ public class Solution {
         return true;
     }
 
+    public int getLucky(String s, int k) {
+        StringBuilder numericalRepresentation = new StringBuilder();
+        for (char c : s.toCharArray()){
+            numericalRepresentation.append(c - 'a'+1);
+        }
+
+        s = numericalRepresentation.toString();
+        while (k-- > 0){
+            int sum = 0;
+            for (char c : s.toCharArray()){
+                sum += c - '0';
+            }
+            s = String.valueOf(sum);
+        }
+        return Integer.parseInt(s);
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        Boolean ans = solution.isAnagram("zeed","deez");
-        System.out.println(ans);
+        /*Boolean ans = solution.isAnagram("zeed","deez");
+        System.out.println(ans);*/
         /*String ok = "dekj";
         String ko = "kjde";
         int[] arr = new int[26];
@@ -27,6 +44,10 @@ public class Solution {
             System.out.println(arr[ok.charAt(i) - 'a']++);
             System.out.println(arr[ko.charAt(i) - 'a']--);
         }*/
+        int ans = solution.getLucky("a",1);
+        System.out.println(ans);
 
     }
 }
+// 1 2 3 4 5 6 7 8 9
+// a b c d e f g h i
